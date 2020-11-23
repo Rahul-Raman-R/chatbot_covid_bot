18dea2beed44950dddf5f09ca1dfb138ba7dc6a3
Rahul-Raman-R <74867210+Rahul-Raman-R@users.noreply.github.com>
2020-11-23 20:42:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 import nltk
 import pickle
+import numpy
 from nltk.stem import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
+from keras.models import load_model
+model = load_model('chatbot_model4.h5')
+import json
 import random
 import numpy as np
-from keras.models import load_model
-model = load_model("chatbot_model4.h5")
 intents = pickle.load(open('intents.pkl','rb'))
 words = pickle.load(open('words4.pkl','rb'))
 classes = pickle.load(open('classes4.pkl','rb'))
@@ -36,12 +38,21 @@ app = Flask(__name__,template_folder='templates')
 def index():
 	return render_template('index.html')
 
-
-@app.route('/get',methods=['GET'])
+@app.route('/get')
 def get_bot_response():
-    message = request.args.get('msg')
-    
-    p = bow(message, words,show_details=False)
+    sentence = request.args.get('msg')
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word.lower()) for word in sentence_words] 
+    # bag of words - matrix of N words, vocabulary matrix
+    bag = [0]*len(words)
+    for s in sentence_words:
+        for i,w in enumerate(words):
+            if w == s:
+                # assign 1 if current word is in the vocabulary position
+                bag[i] = 1
+                
+    p = np.array(bag)
+   
     results = model.predict(np.array([p]))[0]
     results_index = numpy.argmax(results) 
     tag = classes[results_index] 
@@ -57,5 +68,7 @@ def get_bot_response():
    
     return "Missing Data!"
 
+
+
 if __name__ == "__main__":
-	app.run(debug=True)
+	app.run()
