5d62b9d82db8cbcf080e5bfb9d21559bc94bed9a
Rahul-Raman-R <74867210+Rahul-Raman-R@users.noreply.github.com>
2020-11-23 21:14:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import nltk
 import pickle
 import numpy
+from nltk import word_tokenize
 from nltk.stem import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
 from keras.models import load_model
