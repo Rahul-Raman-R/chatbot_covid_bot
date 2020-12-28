3da669a6755039fc42e27d276f26c17976ae97e3
Rahul-Raman-R <74867210+Rahul-Raman-R@users.noreply.github.com>
2020-12-28 16:31:55 +0530

    Update model.py

--- model.py
+++ model.py
@@ -4,18 +4,19 @@ Created on Sun Nov 22 09:58:50 2020
 
 @author: Rahul Raman R
 """
-import numpy as np
-from keras.models import Sequential
-from keras.layers import Dense, Activation, Dropout
-from keras.optimizers import SGD
-import random
-import json
-import pickle
 import nltk
 nltk.download('punkt')
 nltk.download('wordnet')
 from nltk.stem import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
+import json
+import pickle
+
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
+from keras.optimizers import SGD
+import random
 
 
 words=[]
